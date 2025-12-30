b5e5154ec14b6acb5114e12e9521b63475a39817
ricvrdv <costagarcia.r95@gmail.com>
2025-12-30 17:52:48 +0000

    feat(ex00): open input file

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,6 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv) {
-	(void)argv;
 	// 1. Handle no argument
 	if (argc < 2) {
 		std::cerr << "Error: could not open file." << std::endl;
@@ -13,9 +12,33 @@ int main(int argc, char **argv) {
 	if (!loadDatabase("data.csv", btcDatabase)) {
 		return 1;
 	}
-	
-	for (std::map<std::string, double>::iterator it = btcDatabase.begin(); it != btcDatabase.end(); it++) {
-		std::cout << it->first << " => " << it->second << std::endl;
+
+	// 3. Handle input file
+	std::ifstream	inputFile(argv[1]);
+	if (!inputFile.is_open()) {
+		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
 	}
+
+	std::string	line;
+	if (!std::getline(inputFile, line)) {
+		std::cerr << "Error: input file is empty." << std::endl;
+		return 1;
+	}
+
+	if (line.compare("date | value") != 0) {
+		std::cerr << "Error: wrong input file header." << std::endl;
+		return 1;
+	}
+
+	/*for (std::map<std::string, double>::iterator it = btcDatabase.begin(); it != btcDatabase.end(); it++) {
+		std::cout << it->first << " => " << it->second << std::endl;
+	}*/
+
+	while (std::getline(inputFile, line)) {
+		processInputLine(line, btcDatabase);
+	}
+
+	inputFile.close();
 	return 0;
 }
