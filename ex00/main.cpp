440dac023b726148169492e7abac2f53c45fb464
Ricardo Garcia <costagarcia.r95@gmail.com>
2026-01-02 12:24:23 +0000

    feat(ex00): open input file

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,6 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv) {
-	(void)argv;
 	// 1. Handle no argument
 	if (argc < 2) {
 		std::cerr << "Error: could not open file." << std::endl;
@@ -12,6 +11,23 @@ int main(int argc, char **argv) {
 	std::map<std::string, double> btcDatabase;
 	if (!loadDatabase("data.csv", btcDatabase)) {
 		return 1;
+	}
+
+	std::ifstream	inputFile(argv[1]);
+	if (!inputFile.is_open()) {
+		std::cerr << "Error: could not open file." << std::endl;
+        return 1;
+	}
+
+	std::string	line;
+	if (!std::getline(inputFile, line)) {
+		std::cerr << "Error: input file is empty." << std::endl;
+        return 1;
+	}
+
+	if (line.compare("date | value") != 0) {
+		std::cerr << "Error: wrong input file header." << std::endl;
+		return false;
 	}
 	
 	for (std::map<std::string, double>::iterator it = btcDatabase.begin(); it != btcDatabase.end(); it++) {
